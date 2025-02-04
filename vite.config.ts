4d480f696f0022740993018d03a76df5173ee4d0
Nelson Campos <80530709+NelsonArcizetCampos@users.noreply.github.com>
2025-02-04 12:49:12 +0000

    Making it work in CodeSandbox

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   server: {
-    allowedHosts: ["w9gt8t-5173.csb.app"], // Permite qualquer host
+    allowedHosts: ["sx2jj2-5173.csb.app"], // Permite qualquer host
     host: true, // Garante que o Vite aceita conexões externas
   },
 });
